7789039256b00fe0b614ae3c6896ffbba56a8e35
aligh <aligholamzadeh42@gmail.com>
2018-11-22 13:12:43 +0330

    hdfsga

--- home/views.py
+++ home/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home/home.html')
+
+
+def main(request):
+    return render(request, 'home/main.html')
